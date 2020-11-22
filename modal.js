5fd3468c99bf2704ead6799acae2bffa036d88e7
Hartwig Laszlo <laszlo.hartwig@gmail.com>
2020-11-22 18:12:38 +0100

    first modification

--- modal.js
+++ modal.js
@@ -1,5 +1,6 @@
 // Get the modal
 const modal = document.querySelector('.modal');
+const modalContent = document.querySelector('.modal-content')
 
 // Get the button that opens the modal
 const openModal = document.querySelector('.open-modal');
@@ -10,16 +11,19 @@ const toClose = document.querySelector('.close');
 // When the user clicks the button, open the modal 
 openModal.onclick = function() {
   modal.style.display = "block";
+  modalContent.focus();
 }
 
 // When the user clicks on <span> (x), close the modal
 toClose.onclick = function() {
+  modalContent.blur();
   modal.style.display = "none";
 }
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
   if (event.target == modal) {
+    modalContent.blur();
     modal.style.display = "none";
   }
 }
@@ -28,6 +32,7 @@ window.onclick = function(event) {
 const modalButtons = document.querySelectorAll('.modal-buttons')
 for(let i=0; i < modalButtons.length; i+=1) {
   modalButtons[i].onclick = function() {
+    modalContent.blur();
     modal.style.display = "none";
   }
 }
